2ebaef3d3261fe37b4addc5941ea5e66fa3b2c26
Tomas Beranek <tomaber@stud.ntnu.no>
2022-02-04 15:10:07 +0100

    Initializering

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat extends Dog{
+
+    public static boolean dogCat;
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        if (dogCat) {
+            dog.bark();
+            dogCat = false;
+        }
+
+        System.out.println(dog.type);
+    }
+}
---
+++ src/main/java/Dog.java
@@ -0,0 +1,8 @@
+public class Dog{
+
+    protected String type = "labrador";
+
+    public void bark(){
+        System.out.println("woff woff");
+    }
+}
